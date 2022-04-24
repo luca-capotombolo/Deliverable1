4ac986f0972415aac0478e45022bae2cc3454520
luca-capotombolo <capoluca98@gmail.com>
2022-04-25 00:02:52 +0200

    Ho inserito in un unico progetto Excel, Git, Jira e la ricerca dei file del progetto

---
+++ src/main/java/org/example/git/GitSkills.java
@@ -0,0 +1,147 @@
+package org.example.git;
+
+import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.StopWalkException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class GitSkills {
+
+    public static void main(String[] args) throws IOException, GitAPIException {
+        GitSkills gitSkills = new GitSkills("C:\\Users\\lucac\\MyJava\\GitLabSearchCommitByComment", null);
+        //gitSkills.searchCommitByCommentFilter("altroBranch", "altroBranch");
+        gitSkills.searchCommitOfSpecificBranch("myjava");
+        gitSkills.searchCommitOfSpecificBranch("altroBranch");
+        //gitSkills.searchCommitOfAllBranch();
+    }
+
+    private final String localPath;
+    private final String remotePath;
+    private final Repository localRepo;
+    private final Git git;
+
+    public GitSkills(String localPath, String remotePath) throws IOException {
+        this.localPath = localPath;
+        this.remotePath = remotePath;
+        this.localRepo = new FileRepository(localPath + "/.git");
+        this.git = new Git(localRepo);
+    }
+
+    public void cloneRepo(String branch) throws GitAPIException {
+        Git.cloneRepository().setURI(this.remotePath).setBranch(branch).setDirectory(new File(localPath)).call();
+    }
+
+
+    public void searchAllFilesModifiedByACommit(ObjectId treeID){
+        try (TreeWalk treeWalk = new TreeWalk(this.localRepo)) {
+            treeWalk.reset(treeID);
+            while (treeWalk.next()){
+                String path = treeWalk.getPathString();
+                System.out.println(treeWalk.getAttributes().toString());
+                System.out.println(path);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /*
+    Ancora non funziona
+     */
+    public void searchCommitOfAllBranch() throws GitAPIException, IOException {
+        String branchName;
+        List<Ref> branches;
+
+        branches = git.branchList().call();
+
+        for (Ref branch: branches) {
+            branchName = branch.getName();
+
+            System.out.println("Commits del branch: " + branchName);
+            System.out.println("----------------------------------------------------");
+
+            Iterable<RevCommit> commits = git.log().add(this.localRepo.resolve(branchName)).call();
+
+            List<RevCommit> commitsList = new ArrayList<>();
+
+            commits.iterator().forEachRemaining(commitsList::add);
+
+            for(RevCommit commit: commitsList) {
+                System.out.println(commit.getName());
+                System.out.println(commit.getAuthorIdent().getName());
+                System.out.println(new Date(commit.getCommitTime() * 1000L));
+                System.out.println(commit.getFullMessage());
+            }
+
+        }
+
+    }
+
+    public void searchCommitByCommentFilter(String textGrep, String branchName) throws IOException, GitAPIException {
+
+        RevFilter revFilter = new RevFilter() {
+            @Override
+            public boolean include(RevWalk revWalk, RevCommit revCommit) throws StopWalkException {
+                String comment = revCommit.getFullMessage();
+                return comment.contains(textGrep);
+            }
+
+            @Override
+            public RevFilter clone() {
+                return null;
+            }
+        };
+
+
+        Iterable<RevCommit> commits = git.log().add(this.localRepo.resolve(branchName)).setRevFilter(revFilter).call();
+
+        List<RevCommit> commitsList = new ArrayList<>();
+
+        commits.iterator().forEachRemaining(commitsList::add);
+
+        System.out.println("Commit trovati relativi al branch "+ branchName+" contenenti la stringa "+ textGrep+":");
+        System.out.println("--------------------------------------------------------");
+        for(RevCommit commit: commitsList) {
+            System.out.println(commit.getName());
+            System.out.println("Author: "+commit.getAuthorIdent().getName());
+            System.out.println("Date: "+new Date(commit.getCommitTime() * 1000L));
+            System.out.println("Full Message: "+commit.getFullMessage());
+            //ottengo i files che sono stati modificati dal commit
+            ObjectId treeID = commit.getTree().getId();
+            this.searchAllFilesModifiedByACommit(treeID);
+        }
+
+    }
+
+    public void searchCommitOfSpecificBranch(String branchName) throws IOException, GitAPIException {
+
+        Iterable<RevCommit> commits = git.log().add(this.localRepo.resolve(branchName)).call();
+
+        List<RevCommit> commitsList = new ArrayList<>();
+
+        commits.iterator().forEachRemaining(commitsList::add);
+
+        System.out.println("Commit trovati relativi al branch "+ branchName+":");
+        System.out.println("--------------------------------------------------------");
+        for(RevCommit commit: commitsList) {
+            System.out.println(commit.getName());
+            System.out.println("Author: "+commit.getAuthorIdent().getName());
+            System.out.println("Date: "+new Date(commit.getCommitTime() * 1000L));
+            System.out.println("Full Message: "+commit.getFullMessage());
+        }
+
+    }
+}
---
+++ src/main/java/org/example/jira/Jira.java
@@ -0,0 +1,60 @@
+package org.example.jira;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class Jira {
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONArray readJsonArrayFromUrl(String url) throws IOException, JSONException {
+        try (InputStream is = new URL(url).openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String jsonText = readAll(rd);
+            return new JSONArray(jsonText);
+        }
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        try (InputStream is = new URL(url).openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String jsonText = readAll(rd);
+            return new JSONObject(jsonText);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, JSONException {
+
+        String projName ="ZOOKEEPER";		//Progetto dell'ID del ticket di cui siamo interessati
+        Integer j = 0, i = 0, total = 1;
+        //Get JSON API for closed bugs w/ AV in the project
+        do {
+            //Only gets a max of 1000 at a time, so must do this multiple times if bugs >1000
+            j = i + 1000;
+            String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
+                    + projName + "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR"
+                    + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22AND%22"
+                    + "affectedVersion%22=%223.4.5%22&fields=key,resolutiondate,versions,created&startAt="			//KEY = ID
+                    + i + "&maxResults=" + j;
+            JSONObject json = readJsonFromUrl(url);
+            JSONArray issues = json.getJSONArray("issues");
+            total = json.getInt("total");
+            for (; i < total && i < j; i++) {
+                //Iterate through each bug
+                String key = issues.getJSONObject(i%1000).get("key").toString();
+                System.out.println(key); //faccio il print dell'ID
+            }
+        } while (i < total);
+    }
+}
