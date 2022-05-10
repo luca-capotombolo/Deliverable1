f2b39ad5a9ed26383fe8f817b5d571fb9faadd22
luca-capotombolo <capoluca98@gmail.com>
2022-05-10 23:32:01 +0200

    App.java fino alla creazione dell'excel con le metriche escluse.

--- src/main/java/org/capotombolo/App.java
+++ src/main/java/org/capotombolo/App.java
@@ -1,10 +1,10 @@
 package org.capotombolo;
 
+import org.capotombolo.excel.ExcelTools;
 import org.capotombolo.filesystem.FoundAllJavaFiles;
 import org.capotombolo.git.GitSkills;
 import org.capotombolo.jira.Jira;
-//import org.capotombolo.metrics.Size;
-//import org.capotombolo.proportion.Proportion;
+import org.capotombolo.proportion.Proportion;
 import org.capotombolo.utils.Commit;
 import org.capotombolo.utils.Issue;
 import org.capotombolo.utils.MyFile;
@@ -12,10 +12,10 @@ import org.capotombolo.utils.Release;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.RefNotFoundException;
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class App 
@@ -23,8 +23,8 @@ public class App
 
     public static void main(String[] args) throws IOException, GitAPIException {
 
-        final String project = "ZOOKEEPER";
-        final String path = "C:\\Users\\lucac\\ESAME FALESSI\\zookeeper2\\zookeeper";
+        final String project = "BOOKKEEPER";
+        final String path = "C:\\Users\\lucac\\ESAME FALESSI\\bookkeeper";
 
         //Get All Releases from JIRA
         List<Release> releaseList = Jira.getReleases(project);
@@ -43,9 +43,9 @@ public class App
         HashMap<Issue, List<Commit>> hashMapIssueCommits;                               //{Issue1: [commit1, ..., commitN], Issue2: [commit1, ..., commitM], ...}
 
         for (Release release : releaseList) {
-            try{
+            try {
                 gitSkills.setBranch(release.getRelease());
-            }catch (RefNotFoundException e){
+            } catch (RefNotFoundException e) {
                 //inconsistent data on JIRA
                 continue;
             }
@@ -62,47 +62,59 @@ public class App
         //I must order the issues by fixDate
         issueList.sort(Comparator.comparing(o -> o.fixDate));
 
-        //Proportion proportion = new Proportion(releaseList, issueList);
+        Proportion proportion = new Proportion(releaseList, issueList);
 
-        //proportion.increment();
+        proportion.increment();
 
-
-/*
         //Labeling java files
         List<Commit> commits;
         List<MyFile> myFiles;
-        for(Release release: releaseList){
-            for(Issue issue: issueList){
-                if((release.getDate().compareTo(issue.iv.getDate()) >= 0) && (release.getDate().compareTo(issue.fv.getDate()) < 0)){
+        int n = 0, s=0;
+        for (Release release : releaseList) {
+            System.out.println(release.getRelease());
+            if (n == nRelease)
+                break;
+            for (Issue issue : issueList) {
+                if ((release.getDate().compareTo(issue.iv.getDate()) >= 0) && (release.getDate().compareTo(issue.fv.getDate()) < 0)) {
                     commits = hashMapIssueCommits.get(issue);
-                    for(Commit commit: commits){
-                        if(commit.date.compareTo(release.getDate()) > 0){
+                    for (Commit commit : commits) {
+                        if (commit.date.compareTo(release.getDate()) > 0) {
                             myFiles = hashMapReleaseFiles.get(release);
-                            for(MyFile myFile: myFiles){
-                                if(commit.files.contains(myFile.path)){
-                                    myFile.state = MyFile.StateFile.BUG;
+                            for (MyFile myFile : myFiles) {
+                                for(String file: commit.files){
+                                    //System.out.println(file);
+                                    if(myFile.path.contains(file)){
+                                        if(myFile.state!= MyFile.StateFile.BUG) {
+                                            myFile.state = MyFile.StateFile.BUG;
+                                            s++;
+                                            break;
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
-        }*/
+            n++;
+        }
 
-
+        System.out.println(s);
 
         //Compute metrics
         //Size size = new Size(hashMapReleaseFiles, path);
 
-
-        //Scrittura dei dati su Excel
-
-
-
-
-
-
-
-
+        ExcelTools excelTools = new ExcelTools("ISW2", project, "Release", "Name of the class", "Bugginess");
+        boolean ret = excelTools.createTable();
+        if (!ret) {
+            System.out.println("Si è verificato un errore nella creazione della tabella...");
+            System.exit(-1);
+        } else {
+            System.out.println("La tabella è stata creata con successo...");
+        }
+        ret = excelTools.writeData(hashMapReleaseFiles);
+        if (ret){
+            System.out.println("modifica file excel avvenuta con successo...");
+        }
     }
 }
