d6ada9c5673002498ad7fc1a58cb91dcfa6ea934
luca-capotombolo <capoluca98@gmail.com>
2022-05-10 23:28:33 +0200

    Costruzione Excel con labeling delle classi per Bookkeeper e Zookeeper SENZA LE METRICHE

--- src/main/java/org/capotombolo/excel/ExcelTools.java
+++ src/main/java/org/capotombolo/excel/ExcelTools.java
@@ -2,10 +2,15 @@ package org.capotombolo.excel;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
+import org.capotombolo.utils.MyFile;
+import org.capotombolo.utils.Release;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ExcelTools {
@@ -28,7 +33,7 @@ public class ExcelTools {
     public boolean createTable() {
         this.wb = new HSSFWorkbook();
 
-        try (OutputStream fileOut = Files.newOutputStream(Paths.get("ISW2.csv"))) {
+        try (OutputStream fileOut = Files.newOutputStream(Paths.get("ISW2_" + this.cell1 + ".csv"))) {
             Sheet sheet = wb.createSheet(this.sheetName);
             Row titleRow = sheet.createRow(0);
             Cell cell = titleRow.createCell(0);
@@ -52,61 +57,46 @@ public class ExcelTools {
         return true;
     }
 
-    public boolean writeData(List<String> filenames, String release){
+    public boolean writeData(HashMap<Release, List<MyFile>> releaseListHashMap){
 
-        int length = filenames.size();
+        int length = releaseListHashMap.size();
         System.out.println(length);
         if(length==0){
             return false;
         }
 
 
-        try (OutputStream fileOut = Files.newOutputStream(Paths.get("ISW2.csv"))) {
+        try (OutputStream fileOut = Files.newOutputStream(Paths.get("ISW2_" + this.cell1 + ".csv"))) {
             Sheet sheet = this.wb.getSheet(this.sheetName);
             int start = sheet.getLastRowNum();
             Row row;
             Cell cell;
+            List<Release> releaseList = new ArrayList<>();
+            List<MyFile> myFilesForRelease;
+            releaseListHashMap.forEach((release, myFiles) -> {
+                releaseList.add(release);
+            });
             for (int count = 0; count < length; count++) {
-                row = sheet.createRow(start + 1);
-                cell = row.createCell(0);
-                cell.setCellValue(this.cell1);
-                cell = row.createCell(1);
-                cell.setCellValue(release);
-                cell = row.createCell(2);
-                cell.setCellValue(filenames.get(count));
-                System.out.println(filenames.get(count));
-                start += 1;
+                //get all files of release
+                myFilesForRelease = releaseListHashMap.get(releaseList.get(count));
+                for(MyFile myFile: myFilesForRelease){
+                    row = sheet.createRow(start + 1);
+                    cell = row.createCell(0);
+                    cell.setCellValue(this.cell1);
+                    cell = row.createCell(1);
+                    cell.setCellValue(releaseList.get(count).release);
+                    cell = row.createCell(2);
+                    cell.setCellValue(myFile.path);
+                    cell = row.createCell(3);
+                    cell.setCellValue(myFile.state.toString());
+                    start += 1;
+                }
             }
             this.wb.write(fileOut);
         } catch (IOException e) {
             System.out.println(e.getMessage());
             return false;
         }
-
         return true;
     }
-
-
-    public static void main(String[] args) {
-        ExcelTools excelTools = new ExcelTools("ISW2", "ZOOKEEPER", "Release", "Name of the class", "Bugginess");
-        boolean ret = excelTools.createTable();
-
-        if (!ret) {
-            System.out.println("Si è verificato un errore nella creazione della tabella...");
-        }else{
-            System.out.println("La tabella è stata creata con successo...");
-        }
-
-        //FoundAllJavaFiles foundAllJavaFiles = new FoundAllJavaFiles("C:\\Users\\lucac\\maven\\Zookkeeper\\zookeeper");
-        //System.out.println("File contenuti nella cartella: " + foundAllJavaFiles.localPath);
-        //List<String> filenames = foundAllJavaFiles.foundAllFiles();
-
-        //Costruisco la tabella excel
-        //ret = excelTools.writeData(filenames);
-
-        //if (ret){
-        //    System.out.println("modifica file excel avvenuta con successo...");
-        //}
-    }
-
 }
