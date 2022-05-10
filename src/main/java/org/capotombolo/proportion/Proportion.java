53cfa6ea1a655accea027374fb9af6109422a13a
luca-capotombolo <capoluca98@gmail.com>
2022-05-10 23:39:58 +0200

    Implementazione Proportion Increment

--- src/main/java/org/capotombolo/proportion/Proportion.java
+++ src/main/java/org/capotombolo/proportion/Proportion.java
@@ -2,7 +2,6 @@ package org.capotombolo.proportion;
 
 import org.capotombolo.utils.Issue;
 import org.capotombolo.utils.Release;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,19 +17,19 @@ public class Proportion {
 
     public void increment(){
         List<Issue> issueFixedInPrevVersions;
-        List<Float> p_increments = new ArrayList<>();
-        p_increments.add((float) 0);
-        boolean first = true;
+        List<Float> p_increments = new ArrayList<>();           //List of P_Increment for all versions
+        p_increments.add((float) 0);                            //P_increment of first release is 0
+        boolean first = true;                                   //I take the first release
         Release prev = null;
 
-        //for each version, I computed p_increment as the average P among defects fixed in versions 1 to R - 1
+        //for each version, I computed p_increment as the average P among defects fixed in versions from 1 to R - 1
         for(Release release: this.releaseList){
             if(first){
                 prev=release;
                 first=false;
                 continue;
             }
-            //Get all issues that are fixed between release 1 and R - 1
+            //Get all issues fixed between release 1 and R - 1
             issueFixedInPrevVersions = new ArrayList<>();
             for(Issue issue: this.issueList){
                 if(issue.iv!=null && issue.fixDate.compareTo(prev.getDate()) < 0){
@@ -40,7 +39,7 @@ public class Proportion {
                     break;
             }
             float p_issue;
-            float p_increment=0;
+            float p_increment=0;                            //P_Increment of release N
             float fv, iv, ov;
             if(!issueFixedInPrevVersions.isEmpty()){
                 System.out.println("Numero issue fixate nelle versioni precedenti: " + issueFixedInPrevVersions.size());
@@ -48,7 +47,6 @@ public class Proportion {
                     if(issue.ov == issue.fv){
                         fv = this.releaseList.indexOf(issue.fv);
                         iv = this.releaseList.indexOf(issue.iv);
-                        //ov = releaseList.indexOf(issue.ov);
                         p_issue= (fv - iv);
                     }else{
                         fv = this.releaseList.indexOf(issue.fv);
@@ -59,39 +57,33 @@ public class Proportion {
                     p_increment+=p_issue;
                 }
                 p_increment=p_increment/issueFixedInPrevVersions.size();
-                System.out.println("p_increment: " + p_increment);
+                System.out.println("p_increment of release " + release.getRelease() + ": " + p_increment);
             }
             p_increments.add(p_increment);
             prev=release;
         }
-        float p=0;
-        for(Float p_increment: p_increments){
-            p+=p_increment;
-        }
-        p=p/p_increments.size();
-        float iv, ov, fv;
+
+        //Compute IV of issue with inconsistent AV on JIRA
+        float iv, ov, fv, p_increment;
         for(Issue issue: this.issueList){
             if(issue.iv==null){
+                //Issue has no AV on JIRA
                 fv = this.releaseList.indexOf(issue.fv);
                 ov = this.releaseList.indexOf(issue.ov);
+                p_increment = p_increments.get((int)fv);
                 if(issue.ov==issue.fv){
-                    iv = fv - p;
+                    iv = fv - p_increment;
                 }else{
-                    iv = fv - (fv - ov)*p;
+                    iv = fv - (fv - ov)*p_increment;
                 }
                 if(iv + 1> this.releaseList.size()){
                     issue.iv = this.releaseList.get(this.releaseList.size()-1);
+                }else if(iv <= 0){
+                    issue.iv = this.releaseList.get(0);
                 }else{
-                    issue.iv = this.releaseList.get((int)iv +1);
+                    issue.iv = this.releaseList.get((int)iv + 1);
                 }
             }
         }
-    }
-
-
-
-    public static void main(String[] args) {
-        float n = (float) 9.99999999;
-        System.out.println((int)n);
     }
 }
