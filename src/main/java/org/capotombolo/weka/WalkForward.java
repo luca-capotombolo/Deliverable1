76b6119838f85aa5e3625af3a1824372cd67bd53
luca-capotombolo <capoluca98@gmail.com>
2022-06-01 11:13:20 +0200

    rimozione code smell 2 e sostituzione del logger

--- src/main/java/org/capotombolo/weka/WalkForward.java
+++ src/main/java/org/capotombolo/weka/WalkForward.java
@@ -1,6 +1,5 @@
 package org.capotombolo.weka;
 
-import org.apache.logging.log4j.LogManager;
 import org.capotombolo.utils.Release;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
@@ -9,7 +8,8 @@ import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
 import java.util.List;
-import org.apache.logging.log4j.Logger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WalkForward {
 
@@ -29,7 +29,7 @@ public class WalkForward {
         float aucMean = 0;
         float recall = 0;
 
-        Logger logger = LogManager.getLogger(WalkForward.class);
+        Logger logger = Logger.getLogger(WalkForward.class.getName());
 
         //implementing walk-forward...
         for(int count1=0; count1<=releaseList.size()/2; count1++){
@@ -37,10 +37,8 @@ public class WalkForward {
                 continue;
             youngerRelease = releaseList.get(count1-1);
             testingRelease = releaseList.get(count1);
-            logger.error("TESTING RELEASE: " + testingRelease.name);
-            //System.out.println("TESTING RELEASE: " + testingRelease.name);
-            logger.error("YOUNGER RELEASE: " + youngerRelease.name);
-            //System.out.println("YOUNGER RELEASE: " + youngerRelease.name);
+            logger.log(Level.INFO, String.format("TESTING RELEASE: %s", testingRelease.name));
+            logger.log(Level.INFO, "YOUNGER RELEASE: " + youngerRelease.name);
 
             source1 = new ConverterUtils.DataSource("C:\\Users\\lucac\\MyJava\\Deliverable1\\training_"+project+"_"+youngerRelease.name + ".arff");
             training = source1.getDataSet();
@@ -50,50 +48,37 @@ public class WalkForward {
             int numAttr = training.numAttributes();
             training.setClassIndex(numAttr - 1);
             testing.setClassIndex(numAttr - 1);
-            logger.error("NUMBER ATTR: " + numAttr);
-            //System.out.println("NUMBER ATTR: " + numAttr);
+            logger.log(Level.INFO, String.format("NUMBER ATTR: %s", numAttr));
 
             classifierBayes = new NaiveBayes();
             classifierBayes.buildClassifier(training);
             eval = new Evaluation(testing);
             eval.evaluateModel(classifierBayes, testing);
 
-            logger.error("NAIVE BAYES PRECISION: " + eval.precision(0));
-            //System.out.println("NAIVE BAYES PRECISION: " + eval.precision(0));
-            logger.error("NAIVE BAYES RECALL: " + eval.recall(0));
-            //System.out.println("NAIVE BAYES RECALL: " + eval.recall(0));
-            logger.error("NAIVE BAYES AUC = "+eval.areaUnderROC(0));
-            //System.out.println("NAIVE BAYES AUC = "+eval.areaUnderROC(0));
-            logger.error("NAIVE BAYES kappa = "+eval.kappa());
-            //System.out.println("NAIVE BAYES kappa = "+eval.kappa());
+            logger.log(Level.INFO, String.format("NAIVE BAYES PRECISION: %s" , eval.precision(0)));
+            logger.log(Level.INFO, String.format("NAIVE BAYES RECALL: %s",eval.recall(0)));
+            logger.log(Level.INFO, String.format("NAIVE BAYES AUC = %s",eval.areaUnderROC(0)));
+            logger.log(Level.INFO, String.format("NAIVE BAYES kappa = %s",eval.kappa()));
 
             classifierRandomWalker = new RandomForest();
             classifierRandomWalker.buildClassifier(training);
             eval = new Evaluation(testing);
             eval.evaluateModel(classifierRandomWalker, testing);
 
-            logger.error("RANDOM FOREST PRECISION: " + eval.precision(0));
-            //System.out.println("RANDOM FOREST PRECISION: " + eval.precision(0));
-            logger.error("RANDOM FOREST RECALL: " + eval.recall(0));
-            //System.out.println("RANDOM FOREST RECALL: " + eval.recall(0));
-            logger.error("RANDOM FOREST AUC = "+eval.areaUnderROC(0));
-            //System.out.println("RANDOM FOREST AUC = "+eval.areaUnderROC(0));
-            logger.error("RANDOM FOREST kappa = "+eval.kappa());
-            //System.out.println("RANDOM FOREST kappa = "+eval.kappa());
+            logger.log(Level.INFO, String.format("RANDOM FOREST PRECISION: %s" , eval.precision(0)));
+            logger.log(Level.INFO , String.format("RANDOM FOREST RECALL: %s" , eval.recall(0)));
+            logger.log(Level.INFO, String.format("RANDOM FOREST AUC = %s", eval.areaUnderROC(0)));
+            logger.log(Level.INFO, String.format("RANDOM FOREST kappa = %s",eval.kappa()));
 
             classifierIBK = new IBk();
             classifierIBK.buildClassifier(training);
             eval = new Evaluation(testing);
             eval.evaluateModel(classifierIBK, testing);
 
-            logger.error("IBK PRECISION: " + eval.precision(0));
-            //System.out.println("IBK PRECISION: " + eval.precision(0));
-            logger.error("IBK RECALL: " + eval.recall(0));
-            //System.out.println("IBK RECALL: " + eval.recall(0));
-            logger.error("IBK AUC = "+eval.areaUnderROC(0));
-            //System.out.println("IBK AUC = "+eval.areaUnderROC(0));
-            logger.error("IBK kappa = "+eval.kappa());
-            //System.out.println("IBK kappa = "+eval.kappa());
+            logger.log(Level.INFO, String.format("IBK PRECISION: %s" , eval.precision(0)));
+            logger.log(Level.INFO, String.format("IBK RECALL: %s" , eval.recall(0)));
+            logger.log(Level.INFO, String.format("IBK AUC = %s",eval.areaUnderROC(0)));
+            logger.log(Level.INFO, String.format("IBK kappa =%s",eval.kappa()));
 
         }
 
