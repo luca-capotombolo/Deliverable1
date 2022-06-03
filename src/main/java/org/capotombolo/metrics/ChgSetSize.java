fef64f950bbdae0c047cd3b0b95d66902b401108
luca-capotombolo <capoluca98@gmail.com>
2022-06-03 16:45:55 +0200

    risoluzione codesmell 16 refactor ChgSetSize

--- src/main/java/org/capotombolo/metrics/ChgSetSize.java
+++ src/main/java/org/capotombolo/metrics/ChgSetSize.java
@@ -18,39 +18,14 @@ public class ChgSetSize {
     public Map<Release, List<MyFile>> getChgSetSizeRelease()  {
         Release release;
         List<MyFile> myFiles;
-        Set<String>  filesTouchedWithC;
-        boolean touched;
-        int maxNumberTouchedFile;
 
         for (Map.Entry<Release, List<MyFile>> entry : this.hashMap.entrySet()) {
             release = entry.getKey();
             myFiles = entry.getValue();
             for (MyFile myFile : myFiles) {
                 //For each file of release
-                maxNumberTouchedFile = 0;
-                for (CommitMetric commitMetric : commitMetrics) {
-                    //for each commit
-                    if (commitMetric.getRelease().getDate().compareTo(release.getDate()) == 0) {
-                        //all files that has been touched with iterated file
-                        filesTouchedWithC = new HashSet<>();
-                        //I suppose that file is not touched by the commit
-                        touched = false;
-                        for(FileCommitMetric fileCommitMetric: commitMetric.fileCommits){
-                            filesTouchedWithC.add(fileCommitMetric.filename);
-                            if(myFile.path.contains(fileCommitMetric.filename)){
-                                touched = true;
-                            }
-                        }
-                        if(touched){
-                            //myFile is in the set so I must delete it
-                            if(maxNumberTouchedFile < (filesTouchedWithC.size() - 1))
-                                maxNumberTouchedFile = filesTouchedWithC.size() - 1;
-                            myFile.getSetTouchedFileWithCRelease().addAll(filesTouchedWithC);
-                        }
-                    }
-                }
-                //at least zero
-                myFile.setMaxNumberTouchedFile(maxNumberTouchedFile);
+                setChgSetSizeFile(release, myFile);
+
                 try {
                     myFile.setAvgNumberTouchedFile(myFile.getSetTouchedFileWithCRelease().size() / (float) myFile.getNumberRevisionRelease());
                 }catch (Exception e){
@@ -59,6 +34,42 @@ public class ChgSetSize {
             }
         }
         return hashMap;
+    }
+
+    private void setChgSetSizeFile(Release release, MyFile myFile){
+        Set<String>  filesTouchedWithC;
+        boolean touched;
+
+        int maxNumberTouchedFile = 0;
+        for (CommitMetric commitMetric : commitMetrics) {
+            //for each commit
+            if (commitMetric.getRelease().getDate().compareTo(release.getDate()) == 0) {
+                //all files that has been touched with iterated file
+                filesTouchedWithC = new HashSet<>();
+                //I suppose that file is not touched by the commit
+                touched = checkContainsFile(commitMetric, myFile, filesTouchedWithC);
+                if(touched){
+                    //myFile is in the set so I must delete it
+                    if(maxNumberTouchedFile < (filesTouchedWithC.size() - 1))
+                        maxNumberTouchedFile = filesTouchedWithC.size() - 1;
+                    myFile.getSetTouchedFileWithCRelease().addAll(filesTouchedWithC);
+                }
+            }
+        }
+        //at least zero
+        myFile.setMaxNumberTouchedFile(maxNumberTouchedFile);
+    }
+
+    private boolean checkContainsFile(CommitMetric commitMetric, MyFile myFile, Set<String> filesTouchedWithC){
+        boolean touched = false;
+
+        for(FileCommitMetric fileCommitMetric: commitMetric.fileCommits){
+            filesTouchedWithC.add(fileCommitMetric.filename);
+            if(myFile.path.contains(fileCommitMetric.filename)){
+                touched = true;
+            }
+        }
+        return touched;
     }
 
 }
